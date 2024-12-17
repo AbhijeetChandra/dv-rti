14d2d03f77588caf5a90232bd62cbedf0328e2c8
abhijeet_ck <42950575+AbhijeetChandra@users.noreply.github.com>
2024-12-18 05:29:57 +0530

    Angular material integrated

--- employee-management/src/app/app.component.ts
+++ employee-management/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatFormFieldModule, MatDatepickerModule, MatInputModule], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
